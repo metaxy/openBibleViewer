7a56fc125b6daaeeb15487015f3781a8006a95ad
Paul Walger <metaxy@walger.name>
2010-07-25 16:25:08 +0200

    better marklist

--- src/ui/marklist.cpp
+++ src/ui/marklist.cpp
@@ -42,42 +42,19 @@ void MarkList::init()
     connect(ui->tableView, SIGNAL(activated(QModelIndex)), this, SLOT(load(QModelIndex)));
     connect(this, SIGNAL(get(QString)), m_bibleDisplay, SIGNAL(get(QString)));
     QStringList marks;
-    QStringList id = m_notes->getIDList();
+    const QStringList id = m_notes->getIDList();
     for(int i = 0; i < id.size(); ++i) {
         if(m_notes->getType(id.at(i)) == "mark") {
             marks << id.at(i);
         }
     }
-    m_itemModel = new QStandardItemModel(marks.size(), 4);
+    m_itemModel = new QStandardItemModel(marks.size(), 2);
     m_proxyModel = new QSortFilterProxyModel(this);
     m_selectionModel = new QItemSelectionModel(m_proxyModel);
 
-    m_itemModel->clear();
 
     for(int row = 0; row < marks.size(); ++row) {
-
-        QString string;
-        UrlConverter urlConverter(UrlConverter::PersistentUrl, UrlConverter::InterfaceUrl, m_notes->getRef(marks.at(row), "link"));
-        urlConverter.setSettings(m_settings);
-        urlConverter.setModuleMap(m_moduleManager->m_moduleMap);
-        urlConverter.pharse();
-
-        string =  urlConverter.m_bookName + " " + QString::number(urlConverter.m_chapterID + 1) + "," +
-                  QString::number(urlConverter.m_verseID + 1);
-
-        QStandardItem *stelle = new QStandardItem(string);
-        stelle->setData(marks.at(row));
-        m_itemModel->setItem(row, 0, stelle);
-
-
-        QString t;
-        if(m_moduleManager->getModule(urlConverter.m_moduleID) && !m_moduleManager->getModule(urlConverter.m_moduleID)->m_title.isEmpty())
-            t = m_moduleManager->getModule(urlConverter.m_moduleID)->m_title;
-        else
-            t = "";
-        QStandardItem *module = new QStandardItem(t);
-        m_itemModel->setItem(row, 1, module);
-
+        addMark(row,marks.at(row));
     }
     m_proxyModel->setSourceModel(m_itemModel);
     m_proxyModel->setHeaderData(0, Qt::Horizontal, tr("Mark Position"));
@@ -88,13 +65,42 @@ void MarkList::init()
     ui->tableView->setModel(m_proxyModel);
     ui->tableView->setSelectionModel(m_selectionModel);
 }
-void MarkList::load(QModelIndex index)
+void MarkList::addMark(const int &row,const QString &id)
 {
-    UrlConverter urlConverter(UrlConverter::PersistentUrl, UrlConverter::InterfaceUrl, m_notes->getRef(index.data(Qt::UserRole + 1).toString(), "link"));
+    UrlConverter urlConverter(UrlConverter::PersistentUrl, UrlConverter::InterfaceUrl, m_notes->getRef(id, "link"));
     urlConverter.setSettings(m_settings);
     urlConverter.setModuleMap(m_moduleManager->m_moduleMap);
     urlConverter.pharse();
-    QString link = urlConverter.convert();
+
+    const QString string =  urlConverter.m_bookName + " " + QString::number(urlConverter.m_chapterID + 1) + "," +
+              QString::number(urlConverter.m_verseID + 1);
+
+    QStandardItem *stelle = new QStandardItem(string);
+    stelle->setData(id);
+
+    m_itemModel->setItem(row, 0, stelle);
+
+
+    QString t;
+    if(m_moduleManager->getModule(urlConverter.m_moduleID) && !m_moduleManager->getModule(urlConverter.m_moduleID)->m_title.isEmpty())
+        t = m_moduleManager->getModule(urlConverter.m_moduleID)->m_title;
+    else
+        t = "";
+    QStandardItem *module = new QStandardItem(t);
+    m_itemModel->setItem(row, 1, module);
+}
+
+void MarkList::load(QModelIndex index)
+{
+    const int row = index.row();
+    index = m_itemModel->index(row,0);
+    UrlConverter urlConverter(UrlConverter::PersistentUrl,
+                              UrlConverter::InterfaceUrl,
+                              m_notes->getRef(index.data(Qt::UserRole + 1).toString(), "link"));
+    urlConverter.setSettings(m_settings);
+    urlConverter.setModuleMap(m_moduleManager->m_moduleMap);
+    urlConverter.pharse();
+    const QString link = urlConverter.convert();
     emit get(link);
 }
 
@@ -103,12 +109,14 @@ MarkList::~MarkList()
     delete ui;
     delete m_itemModel;
     delete m_proxyModel;
-    delete m_selectionModel;
 }
 void MarkList::showContextMenu(QPoint point)
 {
     QMenu *contextMenu = new QMenu(this);
-    QAction *actionDelete = new QAction(QIcon::fromTheme("edit-delete", QIcon(":/icons/16x16/edit-delete.png")), tr("Delete"), contextMenu);
+    QAction *actionDelete = new QAction(QIcon::fromTheme("edit-delete",
+                                        QIcon(":/icons/16x16/edit-delete.png")),
+                                        tr("Delete"),
+                                        contextMenu);
     connect(actionDelete,SIGNAL(triggered()),this,SLOT(deleteMarks()));
     contextMenu->addAction(actionDelete);
     contextMenu->exec(QCursor::pos());
@@ -117,34 +125,31 @@ void MarkList::showContextMenu(QPoint point)
 void MarkList::addNote(QString id)
 {
     if(m_notes->getType(id) == "mark") {
-/*
-                QStandardItem *parentItem = m_itemModel->invisibleRootItem();
-                QStandardItem *newItem = new QStandardItem;
-                newItem->setText(m_notes->getTitle(id));
-                newItem->setData(id);
-                parentItem->appendRow(newItem);*/
-
+        addMark(m_itemModel->rowCount(),id);
     }
 }
 void MarkList::removeNote(QString id)
 {
-    QModelIndexList list = m_itemModel->match(m_itemModel->index(0, 0), Qt::UserRole + 1, id);
+    QModelIndexList list = m_proxyModel->match(m_proxyModel->index(0, 0),
+                                               Qt::UserRole + 1,
+                                               id);
     if(list.size() != 1) {
         return;
     }
     QModelIndex index = list.at(0);
-    m_itemModel->removeRow(index.row());
+    m_proxyModel->removeRow(index.row());
 }
 void MarkList::deleteMarks()
 {
-    disconnect(m_notes, SIGNAL(noteRemoved(QString,QMap<QString, QString>)), this, SLOT(removeNote(QString)));
+    disconnect(m_notes, SIGNAL(noteRemoved(QString,QMap<QString, QString>)),
+               this, SLOT(removeNote(QString)));
     QModelIndexList list = m_selectionModel->selectedRows();
     if(list.isEmpty()) {
         QModelIndex index = ui->tableView->indexAt(m_currentPoint);
         if(index.isValid()) {
             QString id = index.data(Qt::UserRole + 1).toString();
             m_notes->removeNote(id);
-            m_itemModel->removeRow(index.row());
+            m_proxyModel->removeRow(index.row());
         }
 
     } else {
@@ -152,11 +157,12 @@ void MarkList::deleteMarks()
             QModelIndex index = list.at(0);
             QString id = index.data(Qt::UserRole + 1).toString();
             m_notes->removeNote(id);
-            m_itemModel->removeRow(index.row());
+            m_proxyModel->removeRow(index.row());
             list = m_selectionModel->selectedRows();
         }
     }
-    connect(m_notes, SIGNAL(noteRemoved(QString,QMap<QString, QString>)), this, SLOT(removeNote(QString)));
+    connect(m_notes, SIGNAL(noteRemoved(QString,QMap<QString, QString>)),
+            this, SLOT(removeNote(QString)));
 }
 void MarkList::changeEvent(QEvent *e)
 {
