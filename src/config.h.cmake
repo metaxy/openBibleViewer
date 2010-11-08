d95ed2dd455712a01cf36165822888bbabaa2adc
Paul Walger <metaxy@walger.name>
2010-11-08 17:10:42 +0100

    names

--- src/config.h.cmake
+++ src/config.h.cmake
@@ -3,5 +3,5 @@
 #cmakedefine _PORTABLE_VERSION
 #define _AV_LANG "en;de;ru;cs;tr;sk" 
 #define _AV_LANG_NAME "English;German;Russian;Czech;Turkish;Slovak" 
-#define _AV_LANG_NAME_NATIVE "English;Deutsch;русском;čeština;a;a" 
+#define _AV_LANG_NAME_NATIVE "English;Deutsch;Русский;Česky;Türkmençe;Slovenčina" 
 #endif
