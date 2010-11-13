0fdd1a93f3f6495c58a7d817c066f10cf918ee2f
Paul Walger <metaxy@walger.name>
2010-11-13 09:37:39 +0100

    no patches

--- src/config.h.cmake
+++ src/config.h.cmake
@@ -1,5 +1,6 @@
 #ifndef CONFIG_H
 #define CONFIG_H
+#include <QtCore/QStringList>
 #cmakedefine _PORTABLE_VERSION
 #define _AV_LANG "en;de;ru;cs;tr;sk" 
 #define _AV_LANG_NAME "English;German;Russian;Czech;Turkish;Slovak" 
