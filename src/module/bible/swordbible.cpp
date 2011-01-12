52fc77343199f67d7a3a8bccf872c1c18c58ece4
Paul Walger <metaxy@walger.name>
2011-01-12 14:25:28 +0100

    start: swordbible

---
+++ src/module/bible/swordbible.cpp
@@ -0,0 +1,89 @@
+#include "swordbible.h"
+
+SwordBible::SwordBible()
+{
+}
+void SwordBible::setSettings(Settings *set)
+{
+    m_settings = set;
+}
+/**
+  * Loads the Data.
+  */
+void SwordBible::loadBibleData(const int &id, const QString &path)
+{
+    //DEBUG_FUNC_NAME
+    m_moduleID = id;
+    m_modulePath = path;
+
+
+}
+int SwordBible::readBook(const int &id)
+{
+    m_currentBookID = id;
+    return 0;
+}
+
+QString SwordBible::readInfo(QFile &file)
+{
+
+}
+QString SwordBible::readInfo(const QString &fileName)
+{
+
+}
+
+void SwordBible::search(const SearchQuery &query, SearchResult *res) const
+{
+
+}
+bool SwordBible::hasIndex() const
+{
+
+}
+void SwordBible::buildIndex()
+{
+
+}
+
+int SwordBible::moduleID() const
+{
+    return m_moduleID;
+}
+
+QString SwordBible::modulePath() const
+{
+    return m_modulePath;
+}
+QString SwordBible::moduleName(bool preferShortName) const
+{
+    if(preferShortName) {
+        if(!m_shortModuleName.isEmpty()) {
+            return m_shortModuleName;
+        } else {
+            return m_moduleName;
+        }
+    } else {
+        if(!m_moduleName.isEmpty()) {
+            return m_moduleName;
+        } else {
+            return m_shortModuleName;
+        }
+    }
+}
+
+Book SwordBible::book() const
+{
+
+}
+/**
+  * Returns the path, where all indexed files are stored.
+  */
+QString SwordBible::indexPath() const
+{
+    return m_settings->homePath + "index/" + m_settings->hash(m_modulePath);
+}
+QString SwordBible::uid() const
+{
+    return m_uID;
+}
---
+++ src/module/bible/swordbible.h
@@ -0,0 +1,31 @@
+#ifndef SWORDBIBLE_H
+#define SWORDBIBLE_H
+#include "src/core/dbghelper.h"
+#include "src/module/bible/biblemodule.h"
+
+#include "CLucene.h"
+#include "CLucene/_clucene-config.h"
+
+class SwordBible : public BibleModule
+{
+public:
+    SwordBible();
+    void setSettings(Settings *settings);
+    void loadBibleData(const int &id, const QString &uid);
+    int readBook(const int &id);
+
+    QString readInfo(QFile &file);
+    QString readInfo(const QString &fileName);
+    void search(const SearchQuery &query, SearchResult *res) const;
+    bool hasIndex() const;
+    void buildIndex();
+
+    int moduleID() const;
+    QString modulePath() const;
+    QString moduleName(bool preferShortName = false) const;
+    Book book() const;
+    QString uid() const;
+
+};
+
+#endif // SWORDBIBLE_H
