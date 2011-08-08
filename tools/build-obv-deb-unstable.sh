50662ba1fd7c723bc706a19af48f1757798ef0f5
Paul Walger <metaxy@walger.name>
2011-08-08 13:39:18 +0200

    better tools

--- tools/build-obv-deb-unstable.sh
+++ tools/build-obv-deb-unstable.sh
@@ -1,4 +1,5 @@
-VERSION=0.5.85
+BVERSION=$(git for-each-ref refs/tags --sort=-authordate --format='%(refname)' --count=1)
+VERSION=${BVERSION#"refs/tags/"}
 REPPATH=/home/paul/c++/openBibleViewer
 BUILDPATH=/home/paul/build/home:metaxy:unstable/openbibleviewer
 TAR=openbibleviewer-$VERSION.tar.gz
--- tools/build-obv-unstable.sh
+++ tools/build-obv-unstable.sh
@@ -3,10 +3,10 @@ VERSION=${BVERSION#"refs/tags/"}
 REPPATH=/home/paul/c++/openBibleViewer
 BUILDPATH=/home/paul/build/home:metaxy:unstable/openBibleViewer
 TAR=openBibleViewer-$VERSION.tar.bz2
-rm $BUILDPATH -rf
-mkdir $BUILDPATH
+rm $BUILDPATH/openBibleViewer-*.tar.bz2 -f
+rm $BUILDPATH/openBibleViewer.spec
 cd $REPPATH
 git archive --prefix=openBibleViewer-$VERSION/ HEAD | bzip2 >$TAR
 cp -u $TAR $BUILDPATH
-cp -u $REPPATH/openBibleViewer.spec $BUILDPATH
-rm $TAR
+cp -u $REPPATH/openBibleViewer.spec $BUILDPATH -f
+rm $TAR -f
