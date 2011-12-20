f00faaa0fc42bbdd0260eb56f014130ff994bec9
Paul Walger <metaxy@walger.name>
2011-12-20 21:33:26 +0100

    added tools

---
+++ tools/generate_from_po.sh
@@ -0,0 +1,2 @@
+cd ../src/locales
+lconvert -if po -of ts20 -i po/openbibleviewer-cs.po -o obv_cs.ts
---
+++ tools/generate_qm.sh
@@ -0,0 +1,10 @@
+cd ../src/data
+#lrelease ../locales/obv_cs.ts ../locales/obv_de.ts ../locales/obv_en.ts ../locales/obv_it.ts ../locales/obv_ru.ts ../locales/obv_sk.ts ../locales/obv_tr.ts
+
+lrelease ../locales/obv_cs.ts -qm obv_cs.qm
+lrelease ../locales/obv_de.ts -qm obv_de.qm
+lrelease ../locales/obv_en.ts -qm obv_en.qm
+lrelease ../locales/obv_it.ts -qm obv_it.qm
+lrelease ../locales/obv_ru.ts -qm obv_ru.qm
+lrelease ../locales/obv_sk.ts -qm obv_sk.qm
+lrelease ../locales/obv_tr.ts -qm obv_tr.qm
