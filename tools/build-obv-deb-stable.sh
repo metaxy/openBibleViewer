f3a94118bb0ec90ef62cc5336778cb451635d614
Paul Walger <metaxy@walger.name>
2012-01-11 09:58:22 +0100

    Better deb tools

--- tools/build-obv-deb-stable.sh
+++ tools/build-obv-deb-stable.sh
@@ -2,8 +2,12 @@ VERSION=0.7.1
 REPPATH=/home/paul/c++/openBibleViewer
 BUILDPATH=/home/paul/build/home:metaxy:stable/openbibleviewer
 TAR=openbibleviewer-$VERSION.tar.gz
+rm $BUILDPATH/* -f
 cd $REPPATH
 git archive --prefix=openBibleViewer-$VERSION/ HEAD | gzip >$TAR
 cp -u $TAR $BUILDPATH
 cp -u $REPPATH/deb/* $BUILDPATH -r
+cd $BUILDPATH
+osc ar
+osc commit -m"update"
 
