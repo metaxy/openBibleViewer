1f76e1a0ca93b024b546c3460bb574ee51e0b79c
Paul Walger <metaxy@walger.name>
2012-02-03 20:15:51 +0100

    working on webcommentary

--- src/module/commentary/webcommentary.cpp
+++ src/module/commentary/webcommentary.cpp
@@ -3,3 +3,22 @@
 Webcommentary::Webcommentary()
 {
 }
+Response* Webcommentary::readRanges(const Ranges &ranges, bool ignoreModuleID = false)
+{
+    return NULL;
+}
+int Webcommentary::currentBook()
+{
+    return 0;
+}
+int Webcommentary::currentChapter()
+{
+    return 0;
+}
+std::pair<int, int> Webcommentary::minMaxVerse(const int bookID, const int chapterID)
+{
+    std::pair<int, int> ret;
+    ret.frist = 0;
+    ret.second = 0;
+    return ret;
+}
--- src/module/commentary/webcommentary.h
+++ src/module/commentary/webcommentary.h
@@ -6,7 +6,10 @@ class Webcommentary : public VerseModule
 public:
     Webcommentary();
     Response* readRanges(const Ranges &ranges, bool ignoreModuleID = false);
-
+private:
+    int currentBook();
+    int currentChapter();
+    std::pair<int, int> minMaxVerse(const int bookID, const int chapterID);
 };
 
 #endif // WEBCOMMENTARY_H
