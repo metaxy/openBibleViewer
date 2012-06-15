949d3b722f1b15fd03a857ee6f156f404c0cfea8
Paul Walger <metaxy@walger.name>
2012-06-15 10:35:40 +0200

    Revert "remove includes in ../src/module/commentary/webcommentary.cpp"
    This reverts commit 9beb746e0b57de2052a57abcc5ef38f77110da1d.

--- src/module/commentary/webcommentary.h
+++ src/module/commentary/webcommentary.h
@@ -14,6 +14,9 @@ this program; if not, see <http://www.gnu.org/licenses/>.
 #ifndef WEBCOMMENTARY_H
 #define WEBCOMMENTARY_H
 #include "src/module/commentary/commentarymodule.h"
+#include "src/module/metainfo.h"
+#include <QtScript/QScriptEngine>
+#include <QtCore/QUrl>
 struct WebCommentaryBooksData
 {
     int bookID;
