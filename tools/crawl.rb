3f9184326456b45b4a4a448ee86276fd2d4a15bf
Paul Walger <metaxy@walger.name>
2015-07-08 11:37:27 +0200

    fix module downloads

--- tools/crawl.rb
+++ tools/crawl.rb
@@ -1,26 +1,22 @@
 require 'open-uri'
 require 'nokogiri'
-
+require 'iso-639'
 $base = "http://sourceforge.net"
-$start = "/projects/zefania-sharp/files/Zefania%20XML%20Modules%20%28new%29/"
-
-$end = Array.new()
 $visited = Array.new()
 class Link
     attr_accessor :name, :link, :lang;
 end
-def crawl(top, topname)
+  
+def crawl(top, topname, res, start)
     
     if(top == nil || $visited.index(top) != nil)
         return
     end
-    puts "top = #{top}"
     $visited.push(top);
-    
     doc = Nokogiri::HTML(open(URI.parse(top)))
     links = (doc/'a[href]').map do |a| 
         link = a['href'];
-        if(!(link.end_with?("stats/timeline") || (!link.start_with?($start) && !link.start_with?($base))))
+        if(!(link.end_with?("stats/timeline") || (!link.start_with?(start) && !link.start_with?($base))))
             l = Link.new();
             l.link = link;
             l.name = a.children[0].content.strip;
@@ -34,30 +30,56 @@ def crawl(top, topname)
         if(x.link.end_with?(".zip/download"))
             x.link["/download"] = "";
             lang = x.link;
-            lang[$base+$start] = "";
-            x.link = $base+$start+x.link
+            lang[$base+start] = "";
+            x.link = $base+start+x.link
             x.lang = lang.split("/")[0];
             x.name = topname;
-            $end.push(x)
+            res.push(x)
         else
-
-            crawl($base + x.link, x.name) if not x.link.start_with?("http")
+            crawl($base + x.link, x.name, res, start) if not x.link.start_with?("http")
         end
-          
     end
+    return res
 end
 def main
-    crawl($base+$start, "")
+  
+   
+    
+    
     File.open("modules.xml", "w") do |x|
         x.puts '<?xml version="1.0" encoding="UTF-8"?>';
-        x.puts '<MODULES version="0.1">';
-        x.puts '<ID type="bible">'
-        $end.sort_by! {|x| x.lang }
-        $end.each do |y|
-            x.puts "<ITEM link=\"#{y.link}\" lang=\"#{y.lang}\">#{y.name}</ITEM>";
+        x.puts '<modules version="0.1">';
+        x.puts '<id type="bible">'
+        bibles = crawl($base+"/projects/zefania-sharp/files/Bibles/", nil, Array.new(), "/projects/zefania-sharp/files/Bibles/")
+        bibles.sort_by! {|x| x.lang }
+       
+        bibles.each do |y|
+            lang = ISO_639.find_by_code(y.lang.downcase)
+            if(lang == nil)
+              lang = y.lang
+            else
+              lang = lang.english_name
+            end
+            x.puts "<item link=\"#{y.link}\" lang=\"#{lang}\">#{y.name}</item>";
         end
-        x.puts ' </ID>';
-        x.puts '</MODULES>';
+        
+        x.puts ' </id>';
+        
+        x.puts '<id type="additional">'
+        dict = crawl($base+"/projects/zefania-sharp/files/Dictionaries/", nil, Array.new(), "/projects/zefania-sharp/files/Dictionaries/")
+        dict.sort_by! {|x| x.lang }
+       
+        dict.each do |y|
+            lang = ISO_639.find_by_code(y.lang.downcase)
+            if(lang == nil)
+              lang = y.lang
+            else
+              lang = lang.english_name
+            end
+            x.puts "<item link=\"#{y.link}\" lang=\"#{lang}\">#{y.name}</item>";
+        end
+         x.puts ' </id>';
+        x.puts '</modules>';
     end
 end
 main();
