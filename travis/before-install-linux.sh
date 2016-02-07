07afca5d0db18cabf060a030a86ab448c35e1958
Paul Walger <metaxy@walger.name>
2016-02-07 20:13:39 +0100

    add install of osc

--- travis/before-install-linux.sh
+++ travis/before-install-linux.sh
@@ -11,3 +11,7 @@ cd ..
 export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:$(pwd)/usr/lib/pkgconfig/" 
 export LIBRARY_PATH="${LIBRARY_PATH}:$(pwd)/usr/lib/" 
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:$(pwd)/usr/lib/";
+echo "deb http://download.opensuse.org/repositories/openSUSE:/Tools/xUbuntu_$(lsb_release -rs) ./" | sudo tee /etc/apt/sources.list.d/suse.list
+curl -s "http://download.opensuse.org/repositories/openSUSE:/Tools/xUbuntu_$(lsb_release -rs)/Release.key" | sudo apt-key add -
+sudo apt-get update -qq
+sudo apt-get install -qq --no-install-recommends clang osc
