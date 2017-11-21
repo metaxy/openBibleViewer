27ea3d25417131d93fb95d75eb6e936a4cb7a431
Paul Walger <metaxy@walger.name>
2017-11-21 13:39:40 +0100

    fix tag

--- travis/set-variables.sh
+++ travis/set-variables.sh
@@ -1,5 +1,5 @@
 git fetch --unshallow
-export OBS_VERSION=$(git describe --tags $TRAVIS_BRANCH)
+export OBS_VERSION=$(git describe --abbrev=0 --tags $TRAVIS_BRANCH)
 export OBS_REPPATH=$TRAVIS_BUILD_DIR
 echo $OBS_VERSION
 echo $OBS_REPPATH
