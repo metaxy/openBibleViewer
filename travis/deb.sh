005af7bcbbe1302bf5bfab4a36a1a120fb919304
Paul Walger <metaxy@walger.name>
2017-11-21 16:22:54 +0100

    fix deb build

--- travis/deb.sh
+++ travis/deb.sh
@@ -7,10 +7,9 @@ rm -f openbibleviewer-*.tar.gz 2> /dev/null
 cd $OBS_REPPATH
 echo "Archive File"
 git archive --prefix=openBibleViewer-$OBS_VERSION/ HEAD | gzip >$TAR
-cp -u $TAR $BUILDPATH/openbibleviewer
 cp -u $OBS_REPPATH/deb/* $BUILDPATH -r
+cp -u $TAR $BUILDPATH
 cd $BUILDPATH
-ls $BUILDPATH
 osc ar
 osc commit -m "update to $OBS_VERSION"
 
