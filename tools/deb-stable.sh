bd7ef110375a1e8a87ffc99d7fffd1049b253a9d
Paul Walger <metaxy@walger.name>
2017-11-21 14:14:02 +0100

    add log

--- tools/deb-stable.sh
+++ tools/deb-stable.sh
@@ -4,11 +4,14 @@ REPPATH=/home/paul/coding/openBibleViewer
 BUILDPATH=/home/paul/build/home:metaxy:stable/openbibleviewer
 TAR=openbibleviewer-$VERSION.tar.gz
 cd $BUILDPATH
+echo "Remove Old"
 rm -f openbibleviewer-*.tar.gz 2> /dev/null 
 cd $REPPATH
+echo "Archive File"
 git archive --prefix=openBibleViewer-$VERSION/ HEAD | gzip >$TAR
 cp -u $TAR $BUILDPATH
 cp -u $REPPATH/deb/* $BUILDPATH -r
 cd $BUILDPATH
+ls $BUILDPATH
 osc ar
 osc commit -m"update"
