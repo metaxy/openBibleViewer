2d082d43409e98e789f0a187bb7417bfe7dcf497
Paul Walger <metaxy@walger.name>
2017-11-21 09:30:24 +0100

    try this

--- travis/before-install-linux.sh
+++ travis/before-install-linux.sh
@@ -15,4 +15,4 @@ echo "deb http://download.opensuse.org/repositories/openSUSE:/Tools/xUbuntu_$(ls
 curl -s "http://download.opensuse.org/repositories/openSUSE:/Tools/xUbuntu_$(lsb_release -rs)/Release.key" | sudo apt-key add -
 
 sudo apt-get update -qq
-sudo apt-get install -qq --no-install-recommends clang osc qt5-default qttools5-dev-tools qtbase5-dev qtscript5-dev qtwebengine5-dev libqt5webkit5-dev
+sudo apt-get install -qq --no-install-recommends clang osc qt5-default qttools5-dev-tools
