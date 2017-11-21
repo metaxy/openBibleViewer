8f406fa2e30d0ec40fc48dcc392b10169cfe9160
Paul Walger <metaxy@walger.name>
2017-11-21 17:01:40 +0100

    fix cache?

--- travis/before-install-osx.sh
+++ travis/before-install-osx.sh
@@ -2,8 +2,8 @@ export COLUMNS=80
 curl -LO https://raw.githubusercontent.com/GiovanniBussi/macports-ci/master/macports-ci
 chmod +x ./macports-ci
 ./macports-ci install
-./macports-ci ccache
 PATH="/opt/local/bin:$PATH"
+
 sudo ./macports-ci ccache
 sudo port install qt5 qt5-qtwebkit qt5-qtscript
 sudo ./macports-ci ccache --save
