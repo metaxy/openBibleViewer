68af7de2684d0e8a57a46606a5766062d52688bb
Paul Walger <metaxy@walger.name>
2017-11-21 09:11:55 +0100

    add qt5 to travis

--- travis/before-install-linux.sh
+++ travis/before-install-linux.sh
@@ -13,5 +13,8 @@ export LIBRARY_PATH="${LIBRARY_PATH}:$(pwd)/usr/lib/"
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:$(pwd)/usr/lib/";
 echo "deb http://download.opensuse.org/repositories/openSUSE:/Tools/xUbuntu_$(lsb_release -rs) ./" | sudo tee /etc/apt/sources.list.d/suse.list
 curl -s "http://download.opensuse.org/repositories/openSUSE:/Tools/xUbuntu_$(lsb_release -rs)/Release.key" | sudo apt-key add -
+sudo add-apt-repository --yes ppa:beineri/opt-qt57-trusty
 sudo apt-get update -qq
-sudo apt-get install -qq --no-install-recommends clang osc
+sudo apt-get install -qq --no-install-recommends clang osc qt57-meta-full
+QT_ENV_SCRIPT=$(find /opt -name 'qt*-env.sh')
+source $QT_ENV_SCRIPT
