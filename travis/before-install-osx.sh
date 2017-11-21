fba8e71d1a3e6550707f3a562c489f5021893e32
Paul Walger <metaxy@walger.name>
2017-11-21 13:13:34 +0100

    ccache

--- travis/before-install-osx.sh
+++ travis/before-install-osx.sh
@@ -2,9 +2,9 @@ export COLUMNS=80
 curl -LO https://raw.githubusercontent.com/GiovanniBussi/macports-ci/master/macports-ci
 chmod +x ./macports-ci
 ./macports-ci install
+./macports-ci ccache
 PATH="/opt/local/bin:$PATH"
-brew update && brew install clucene
-
+./macports-ci ccache
 sudo port install qt5
-port contents qt5
-ls /usr/local/opt/qt5
+ ./macports-ci ccache --save
+port contents qt5-qtbase
