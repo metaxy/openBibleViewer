9f10d72f9411dac299131213bedde18e1a3618b1
Paul Walger <metaxy@walger.name>
2017-12-08 14:24:02 +0100

    working on travis deps #14

--- travis/before-install-linux.sh
+++ travis/before-install-linux.sh
@@ -1,13 +1,3 @@
-echo "Buildung CLucene"
-wget http://downloads.sourceforge.net/clucene/clucene-core-2.3.3.4.tar.gz 
-tar -zxvf clucene-core-2.3.3.4.tar.gz 
-cd clucene-core-2.3.3.4/ 
-mkdir build 
-cd build 
-cmake -DCMAKE_INSTALL_PREFIX='../../usr' -DBUILD_CONTRIBS_LIB=ON .. 
-make 
-make install 
-
 cd .. 
 cd .. 
 export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:$(pwd)/usr/lib/pkgconfig/" 
@@ -17,4 +7,4 @@ echo "deb http://download.opensuse.org/repositories/openSUSE:/Tools/xUbuntu_$(ls
 curl -s "http://download.opensuse.org/repositories/openSUSE:/Tools/xUbuntu_$(lsb_release -rs)/Release.key" | sudo apt-key add -
 
 sudo apt-get update -qq
-sudo apt-get install -qq --no-install-recommends clang osc libquazip-dev libquazip-dev zlib1g-dev qt5-default qttools5-dev-tools qtscript5-dev  libqt5webkit5-dev qtwebengine5-dev
+sudo apt-get install -qq --no-install-recommends clang osc libquazip-dev libquazip-dev zlib1g-dev qt5-default qttools5-dev-tools qtscript5-dev  libqt5webkit5-dev libqt5webengine5-dev libclucene-core1
