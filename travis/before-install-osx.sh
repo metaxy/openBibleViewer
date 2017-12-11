17b7360761ed7be83f98918dbc856a332d0d9074
Paul Walger <metaxy@walger.name>
2017-12-11 15:48:03 +0100

    strange mac

--- travis/before-install-osx.sh
+++ travis/before-install-osx.sh
@@ -5,7 +5,6 @@ chmod +x ./macports-ci
 ./macports-ci install
 PATH="/opt/local/bin:$PATH"
 sudo ./macports-ci ccache
-sudo port install qt5-qtbase bzip2 qt5-qtmacextras qt5-qtwebkit qt5-qtscript zlib
-# sudo port install qt5-qtwebengine
+sudo port install qt5-qtbase bzip2 qt5-qtwebkit qt5-qtscript zlib
 sudo ./macports-ci ccache --save
 brew update && brew install clucene quazip
