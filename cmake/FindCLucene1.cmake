3d92f826e03d0fdaa81283db1d12842e06ab9881
Paul Walger <metaxy@walger.name>
2017-12-11 13:33:17 +0100

    do not use pkg for clucene #28

--- cmake/FindCLucene1.cmake
+++
@@ -1,4 +0,0 @@
-# This module looks for clucene (http://clucene.sf.net) support
-# It will define the pkgconfig values with a CLUCENE1_ prefix... 
-include(FindPkgConfig)
-pkg_check_modules(CLUCENE1 REQUIRED libclucene-core)
