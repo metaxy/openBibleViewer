2c637ba387ef1125c97c66fa83b9572a387fb9e4
Paul Walger <metaxy@walger.name>
2017-12-11 14:15:59 +0100

    fix for other plattforms

---
+++ cmake/FindCLucene1.cmake
@@ -0,0 +1,4 @@
+# This module looks for clucene (http://clucene.sf.net) support
+# It will define the pkgconfig values with a CLUCENE1_ prefix... 
+include(FindPkgConfig)
+pkg_check_modules(CLUCENE1 REQUIRED libclucene-core)
